6d45777e486fc126773156b96cf96cbc87df0c4b
johnrhampton <johnrhampton@gmail.com>
2017-02-18 15:09:56 -0500

    manage app window closed event

--- src/app.js
+++ src/app.js
@@ -6,11 +6,19 @@ const electron = require('electron');
 const app = electron.app;
 // submodule :: create and control browser windows
 const BrowserWindow = electron.BrowserWindow;
+// electron app window
+let appWindow = null;
 
 // electron onReady event
 app.on('ready', () => {
-  new BrowserWindow({
+  appWindow = new BrowserWindow({
     height: 500,
     width: 500
+  });
+
+  // manage app window closed event
+  appWindow.on('closed', () => {
+    console.log('electron app closed');
+    appWindow = null;
   });
 });
