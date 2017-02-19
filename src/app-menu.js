6c736fc3f8ce8b7153506dd428c551da57f7177d
johnrhampton <johnrhampton@gmail.com>
2017-02-18 21:26:47 -0500

    about dialog, separator, quit button

--- src/app-menu.js
+++ src/app-menu.js
@@ -1,4 +1,6 @@
 const electron = require('electron');
+// submodule :: application event lifecycle
+const app = electron.app;
 
 // retrieve product name from package.json
 // OS X convention to use this as main menu item
@@ -10,7 +12,23 @@ module.exports = [
   {
     label: productName,
     submenu: [
-      { label: `About ${productName}` }
+      {
+        label: `About ${productName}`,
+        // add logic based on role :: http://electron.atom.io/docs/api/menu-item/
+        // when role is specified on macOS, all other options are ignored
+        // about is macOS only
+        role: 'about'
+      },
+      // add a horizontal separator
+      {
+        type: 'separator'
+      },
+      // add a quit button w/ keyboard shorcut
+      {
+        label: `Quit ${productName}`,
+        click: () => app.quit(),
+        accelerator: 'Cmd+Q'
+      }
     ]
   }
 ];
