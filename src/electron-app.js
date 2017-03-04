e1a8316aa4906a0a5de53191cb2f8d098a15dfba
johnrhampton <johnrhampton@gmail.com>
2017-03-04 11:11:50 -0500

    loadURL uses localhost:3000, open devTools, resizable: false, all-closed event, rename to electron-*

--- src/electron-app.js
+++ src/electron-app.js
@@ -8,8 +8,11 @@
  */
 const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
+// node
+const path = require('path');
+const url = require('url');
 // menu template
-const menuTemplate = require('./app-menu');
+const menuTemplate = require('./electron-app-menu');
 // electron app window
 let appWindow = null;
 
@@ -18,7 +21,8 @@ app.on('ready', () => {
   // instantiate browser window
   appWindow = new BrowserWindow({
     height: 500,
-    width: 500
+    width: 500,
+    resizable: false
   });
 
   // build application menu
@@ -28,11 +32,25 @@ app.on('ready', () => {
 
   // load the main view using the file protocol
   // we have access to the file system as we are running on the local OS
-  appWindow.loadURL(`file://${__dirname}/beer-journal/beer-journal.html`);
+  appWindow.loadURL('http://localhost:3000');
+
+  // if we are not in production, open the devtools
+  if (process.env.NODE_ENV !== 'production') {
+    appWindow.webContents.openDevTools();
+  }
 
   // manage app window closed event
   appWindow.on('closed', () => {
     console.log('electron app closed');
     appWindow = null;
   });
+});
+
+// handle all windows are closed event
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
