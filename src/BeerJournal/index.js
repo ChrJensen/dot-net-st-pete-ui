ff0c0d9ecfb6c095fc8e183c9c1da893f9cd6a48
johnrhampton <johnrhampton@gmail.com>
2017-03-04 23:34:33 -0500

    beer-journal -> BeerJournal

---
+++ src/BeerJournal/index.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+import './beer-journal.css';
+
+export default class BeerJournal extends PureComponent {
+
+  render() {
+    return (
+      <div>
+        <h2>beer me!</h2>
+      </div>
+    );
+  }
+}
--- src/beer-journal/index.js
+++
@@ -1,1 +0,0 @@
-console.log('beer journal feature!');
